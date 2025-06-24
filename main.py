4f934cbaf58d83b22ce193447f0003fd488480bf
ryan <ryan.kimani0@gmail.com>
2025-06-24 17:21:41 +0800

    Created Vector database using Chroma, Used a local embedding model (sentence-transformers) instead of OpenAI

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from sentence_transformers import SentenceTransformer
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+import shutil
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
@@ -22,4 +26,25 @@ def split_text(documents):
 documents = load_documents()
 chunks = split_text(documents)
 
-print(f"Loaded {len(documents)} documents and split into {len(chunks)} chunks.")
+#print(f"Loaded {len(documents)} documents and split into {len(chunks)} chunks.")
+
+CHROMA_PATH = "chroma"
+
+def save_to_chroma(chunks):
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    embedding_function = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    db = Chroma.from_documents(
+        chunks,
+        embedding_function,
+        persist_directory=CHROMA_PATH
+    )
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+
+save_to_chroma(chunks)
+
